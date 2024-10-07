fb30e3a961a9be481f30d10a442122f5ba140e30
Wemerson <104464550+wemersonolvr@users.noreply.github.com>
2024-10-07 18:46:01 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+// src/App.js
+
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Home from './components/pages/home';
+import './App.css';
 
 function App() {
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
