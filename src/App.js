314049e5423174f30f279cf5094d8ceefa34f4a0
Wemerson <104464550+wemersonolvr@users.noreply.github.com>
2024-10-07 11:05:29 -0300

    Ajeitando Login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-// src/App.js
-
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Home from './components/pages/home';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
     </Router>
   );
 }
